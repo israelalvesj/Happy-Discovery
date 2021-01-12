067c5e3b31b7a5d6ba2d1fe64296b826c97421b5
israelalvesj <israel.alves05@gmail.com>
2021-01-12 00:50:48 -0300

    Início aula 04

---
+++ src/database/fakedata.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-7.207001",
+        lng: "-48.218559",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1603138461768-23d1ba2ab00c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1602133187081-4874fdbd555c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1606461048073-ef2df26cf660?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1605506412358-303da9ea238a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ],
+
+    },
+
+    {
+        id:2,
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            ""
+        ]
+    }
+]
