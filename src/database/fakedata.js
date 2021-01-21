a8c19879e8ff05c49a6b37e38087ea4e86416026
israelalvesj <israel.alves05@gmail.com>
2021-01-21 00:35:06 -0300

    continuação aula 04

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -14,15 +14,30 @@ module.exports = [
 
             "https://images.unsplash.com/photo-1605506412358-303da9ea238a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
         ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
 
     },
 
     {
-        id:2,
-        name: "Lar das meninas",
+        id: 2,
+        lat: "-7.1936897",
+        lng: "-48.216699",
+        name: "Lar dos meninos",
         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [
-            ""
-        ]
-    }
+            "https://images.unsplash.com/photo-1603138461768-23d1ba2ab00c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1602133187081-4874fdbd555c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1606461048073-ef2df26cf660?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+
+            "https://images.unsplash.com/photo-1605506412358-303da9ea238a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+
+    },
 ]
